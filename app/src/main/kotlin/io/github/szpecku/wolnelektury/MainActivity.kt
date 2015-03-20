cd37397aa0df9bb46b3a574fbd192f8dea5659a4
Krzysztof Skrzynecki <krzysztof.skrzynecki@vidzone.tv>
2015-03-20 06:49:46 +0100

    Init repository

---
+++ app/src/main/kotlin/io/github/szpecku/wolnelektury/MainActivity.kt
@@ -0,0 +1,15 @@
+package io.github.szpecku.wolnelektury
+
+import android.support.v7.app.ActionBarActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+
+
+public class MainActivity : ActionBarActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
